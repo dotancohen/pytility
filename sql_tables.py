7fd254659da18714f8f65a5653a36bd2c698346b
Dotan Cohen <github-com-2010.04@dotancohen.com>
2017-05-28 13:37:20 +0300

    Put logic in main() method

--- sql_tables.py
+++ sql_tables.py
@@ -8,16 +8,25 @@ Curently only supports dropping tables
 
 import sys
 
-skip = ('rows in set', 'Tables_in_', '----',)
 
-print("Paste the output of 'show tables;' here then press Ctrl-D");
-data = sys.stdin.readlines()
-print("\n")
+def main():
 
-for i in data:
-	table = i[2:-2].strip()
-	if table=='' or table.startswith(skip):
-		continue
-	print("drop table %s;" % table)
+	skip = ('rows in set', 'Tables_in_', '----',)
 
-print("\nshow tables;\n")
+	print("Paste the output of 'show tables;' here then press Ctrl-D");
+	data = sys.stdin.readlines()
+	print("\n")
+
+	for i in data:
+		table = i[2:-2].strip()
+		if table=='' or table.startswith(skip):
+			continue
+		print("drop table %s;" % table)
+
+	print("\nshow tables;\n")
+
+
+
+if __name__ == "__main__":
+
+	main();
